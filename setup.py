6ec753f79f31d0b847569ab40478e11d5de8f2a8
Mike Austin <mike2010@austin-uk.co.uk>
2012-03-29 09:52:25 +0100

    completed steps for pypi registration

--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
-from distutils.core import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
 
 setup(
     name='pydotmailer',
-    version=__import__('pydotmailer').__version__,
+    version='0.1',
     description='A reusable python module for driving the dotMailer API',
-    author='Mike Austin',
+    author='Mike Austin at Triggered Messaging',
     author_email='mike.austin2012@triggeredmessaging.com',
     url='http://github.com/TriggeredMessaging/pydotmailer/',
-    packages=[
-        'pydotmailer',
+    packages=find_packages(exclude=['examples', 'examples.*', "*.tests", "*.tests.*", "tests.*", "tests"]),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
     ],
     package_dir={'pydotmailer': 'pydotmailer'},
+    zip_safe=False,
+    tests_require=[]
 )
