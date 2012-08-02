1eec77f2f8a8b68af387edd8b5f8f97fb1e81869
Mike Austin <mike2010@austin-uk.co.uk>
2012-08-02 08:48:15 +0100

    added suds plugin to manipulate the xml gemerated by add_contact_to_address_book

--- pydotmailer.py
+++ pydotmailer.py
@@ -29,6 +29,47 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+from suds.plugin import *
+class DotMailerSudsPlugin(MessagePlugin):
+    """ case 649
+    plugin for SUDS which can inspect and manipulate the xml packets sent to/from the soap server.
+    docs here https://fedorahosted.org/suds/wiki/Documentation#MessagePlugin
+    """
+    def marshalled(self, context):
+        """ member function which is called by SUDS when it starts to render/marshall the
+        xml to send to the server. At this stage, we have an in-memory set of objects 
+        starting with the SOAP context. Any chances we make to the nest of objects
+        will then be rendered to XML by our caller. 
+        """
+
+
+        try:
+            context.envelope.set('xmlns:apic','http://apiconnector.com')
+            context.envelope.set('xmlns:xsd','http://www.w3.org/2001/XMLSchema')
+
+            body = context.envelope.getChild('Body')
+            if body:
+                add_contact_to_address_book = body.getChild('AddContactToAddressBook')
+                if add_contact_to_address_book:
+                    # We need to manipulate the "Values" sub-sub-node to set xsi:type="xsd:string" on each element. i.e.
+                    # we start with: <apic:anyType >Jim</apic:anyType>
+                    # we need: <apic:anyType xsi:type="xsd:string">Jim</apic:anyType>
+
+                    # The following nest of objects should all exist. Worst-case, any exception will be caught.
+                    contact = add_contact_to_address_book.getChild('contact')
+                    data_fields = contact.getChild('DataFields')
+                    values = data_fields.getChild('Values')
+                    for value in values:
+                        value.set('xsi:type','xsd:string')
+
+
+            #foo = body[0]
+            #foo.set('id', '12345')
+            #foo.set('version', '2.0')
+        except:
+            logger.exception("Exception in DotMailerSudsPlugin::marshalled")
+
+
 class PyDotMailer(object):
     version = '0.1'
     
@@ -61,7 +102,7 @@ class PyDotMailer(object):
 
         # log connection as time can be significant.
         logger.debug("Connecting to web service")
-        self.client = SOAPClient(self.api_url)
+        self.client = SOAPClient(self.api_url, plugins=[DotMailerSudsPlugin()])
         logger.debug("Connected to web service")
         self.api_username = api_username
         self.api_password = api_password
@@ -255,7 +296,7 @@ class PyDotMailer(object):
             if dict_result.get('error_code') == PyDotMailer.ERRORS.ERROR_CONTACT_NOT_FOUND:
                 pass # ignore these expected errors
             else:
-                # record the unexpected exception
+
                 logger.exception("Exception in GetContactByEmail")
         return dict_result
 
